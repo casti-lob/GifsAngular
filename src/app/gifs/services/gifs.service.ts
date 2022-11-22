163570730a7a29e5e008953e4d082bf5cc631e2b
casti-lob <casti_lob@hotmail.com>
2022-11-22 19:00:13 +0100

    añadido gifs a la busqueda

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,17 +1,22 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, GifsSearchResponse } from '../interfaces/searchResponse.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
-  constructor() { 
+  constructor( private http: HttpClient) { 
 
   }
 
   private _history:string []= [
     
   ]
+  private api_key: string ='uRFwIxNEZZu1eLNmm8WY44yj8M3nOQth';
+  private url: string='https://api.giphy.com/v1/gifs/search';
+  results: Gif[]=[]
   get history():string[]{
     return [...this._history]
   }
@@ -31,6 +36,15 @@ export class GifsService {
     //     this._history.unshift(query)
     //   }
     // }
-   
+    
+    const params = new HttpParams()
+    .set('api_key',this.api_key)
+    .set('q',query)
+
+    this.http.get<GifsSearchResponse>(this.url, {params})
+    .subscribe((resp)=>this.results= resp.data
+    )
   }
+
+  
 }
